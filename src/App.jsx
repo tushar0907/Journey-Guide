1b24308132a2dac1e22e6f7931e763ffef16188a
Elite Programmer <tusharkumar0807@gmail.com>
2022-12-01 03:28:49 +0530

    adding features

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css';
 import React from "react";
+import {CssBaseline, Grid} from '@mui/material';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello Neha
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{width: "100%"}}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+          <Map />
+        </Grid>
+
+      </Grid>
+
+    </>
   );
 }
 
