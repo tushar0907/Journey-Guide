8fe7277179d8402e5f6c686c23f448a9d113b0ef
Elite Programmer <tusharkumar0807@gmail.com>
2022-12-01 11:45:56 +0530

    components work

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Autocomplete} from '@react-google-maps/api';
 import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core';
-// import SearchIcon from '@material-ui/icons/Search';
+import SearchIcon from '@material-ui/icons/Search';
 
 import useStyles from './styles.js';
 
@@ -18,14 +18,14 @@ const Header=({onPlaceChanged, onLoad}) => {
                               <Typography variant="h6" className={classes.title}>
                                     Explore new places
                               </Typography>
-                              <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
-                                    <div className={classes.search}>
-                                          {/* <div className={classes.searchIcon}>
-                                                <SearchIcon />
-                                          </div> */}
-                                          <InputBase placeholder="Search…" classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                              {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}> */}
+                              <div className={classes.search}>
+                                    <div className={classes.searchIcon}>
+                                          <SearchIcon />
                                     </div>
-                              </Autocomplete>
+                                    <InputBase placeholder="Search…" classes={{root: classes.inputRoot, input: classes.inputInput}} />
+                              </div>
+                              {/* </Autocomplete> */}
                         </Box>
                   </Toolbar>
             </AppBar>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import {Paper, Typography, useMediaQuery} from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+// import Rating from '@material-ui/lab';
 
-const Map=() => {
+// import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
+import {Rating} from '@mui/material';
+
+
+
+const Map=({coords, places, setCoords, setBounds, setChildClicked, weatherData}) => {
+      const matches=useMediaQuery('(min-width:600px)');
+      const classes=useStyles();
+
+      const coordinates={lat: 0, lng: 0};
       return (
-            <div>Map</div>
+            <div className={classes.mapContainer}>
+                  <GoogleMapReact
+                        bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAP_API_KEY}}
+                        defaultCenter={coordinates}
+                        center={coordinates}
+                        defaultZoom={14}
+                        margin={[50, 50, 50, 50]}
+                        options={''}
+                        onChange={''}
+                        onChildClick={''}
+                  >                        {/* {places.length&&places.map((place, i) => (
+                              <div
+                                    className={classes.markerContainer}
+                                    lat={Number(place.latitude)}
+                                    lng={Number(place.longitude)}
+                                    key={i}
+                              >
+                                    {!matches
+                                          ? <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                                          :(
+                                                <Paper elevation={3} className={classes.paper}>
+                                                      <Typography className={classes.typography} variant="subtitle2" gutterBottom> {place.name}</Typography>
+                                                      <img
+                                                            className={classes.pointer}
+                                                            src={place.photo? place.photo.images.large.url:'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                                                      />
+                                                      <Rating name="read-only" size="small" value={Number(place.rating)} readOnly />
+                                                </Paper>
+                                          )}
+                              </div>
+                        ))}
+                        {weatherData?.list?.length&&weatherData.list.map((data, i) => (
+                              <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+                                    <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" />
+                              </div>
+                        ))} */}
+                  </GoogleMapReact>
+            </div>
       )
 }
 
